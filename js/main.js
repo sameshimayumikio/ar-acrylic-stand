597f5a4abdc34cde109c94f22b979c4d5da13446
sameshimayumikio <yumikio@hotmail.co.jp>
2026-08-06 16:30:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ async function startAR() {
 
   const video = document.createElement('video');
   video.src = 'assets/videos/dance.mp4';
-  video.loop = true;
   video.playsInline = true;
   video.crossOrigin = 'anonymous';
   video.setAttribute('webkit-playsinline', '');
