484c74ca8c8f1b8c17c52a381087cd32904074ee
sameshimayumikio <yumikio@hotmail.co.jp>
2026-09-20 12:02:29 +0900

    Update production.js

--- js/production.js
+++ js/production.js
@@ -17,7 +17,7 @@ startButton.addEventListener('click', async () => {
 async function startAR() {
   const mindarThree = new MindARThree({
     container: document.querySelector('#ar-container'),
-    imageTargetSrc: '../assets/01/05-600px-targets.mind',
+    imageTargetSrc: '../assets/01/05-1000px-targets.mind',
     warmupTolerance: 1,
     missTolerance: 60,
     filterMinCF: 0.00001,
