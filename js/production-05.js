61194af9d4ccc56e1faa56b647f8fd799f1a8e6f
sameshimayumikio <yumikio@hotmail.co.jp>
2026-09-20 22:55:33 +0900

    Update production-05.js

--- js/production-05.js
+++ js/production-05.js
@@ -73,8 +73,8 @@ async function startAR() {
   // 05番のRGB側 447×644px に合わせる（644 / 447 ≒ 1.4407）
   const geometry = new THREE.PlaneGeometry(1, 1.4407);
   const plane = new THREE.Mesh(geometry, material);
-　plane.scale.set(1.2, 1.2, 1);
-　anchor.group.add(plane);
+  plane.scale.set(1.2, 1.2, 1);
+  anchor.group.add(plane);
 
   anchor.onTargetFound = () => {
     video.currentTime = 0;
